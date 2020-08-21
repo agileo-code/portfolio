1f13758226125e7fcd5126d5d72e443eed4c387b
rubenofen <rubenofen@gmail.com>
2020-08-21 18:16:06 +0200

    Bigger title

--- components/sections/mainSlide.js
+++ components/sections/mainSlide.js
@@ -5,7 +5,7 @@ const MainSlide = () => (
   <SectionLayout className="h-screen bg-corporative-slideDark text-corporative-color">
     <div className="grid lg:grid-cols-2 flex-1">
       <div className="self-center">
-        <h1>RUSHMORE CODE</h1>
+        <h1 className="text-6xl">RUSHMORE CODE</h1>
         <h5>The future is here, come in and pay for it.</h5>
         <span>Show me the money.</span>
       </div>
