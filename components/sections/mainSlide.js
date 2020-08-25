1c2e52d55042734e75160fa793b0f7ae0018cc50
rubenofen <rubenofen@gmail.com>
2020-08-25 16:22:47 +0200

    Playing with name

--- components/sections/mainSlide.js
+++ components/sections/mainSlide.js
@@ -6,9 +6,11 @@ const MainSlide = () => (
   <SectionLayout className="h-screen bg-corporative-slideDark text-corporative-color">
     <div className="grid lg:grid-cols-2 flex-1">
       <div className="self-center">
-        <h1 className="text-6xl">Agileo CODE</h1>
-        <h5>Latest software tools for you.</h5>
-        <span>Show me the money.</span>
+        <h1 className="text-6xl">
+          <span className="text-blue-500">A</span>gileo {`<CODE /`}
+          <span className="text-blue-500">{`>`}</span>
+        </h1>
+        <h5>The power of latest software technology for you.</h5>
       </div>
       <Universe className="self-center" />
     </div>
