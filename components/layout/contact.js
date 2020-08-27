6f9253a7a51fba058ed4ea9f03393472433b721e
dastasoft <dastasoft@gmail.com>
2020-08-28 00:57:26 +0200

    contact form working with fromspree

--- components/layout/contact.js
+++ components/layout/contact.js
@@ -1,27 +1,71 @@
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+
 import SectionLayout from '../sectionLayout';
 
-const Contact = () => (
-  <SectionLayout id="contact" className="items-center bg-blue-600 text-black">
-    <h2>Do you have a project in mind?</h2>
-    <span>Write us. Together we can do something great.</span>
-    <form className="m-10 max-w-2xl flex flex-col w-full items-center">
-      <div className="w-full grid gap-3 text-black">
-        <input className="placeholder-black p-2" placeholder="Name*" />
-        <input className="placeholder-black p-2" placeholder="Phone number" />
-        <input className="placeholder-black p-2" placeholder="Email*" />
-        <textarea
-          className="placeholder-black p-2"
-          placeholder="Tell us briefly about your project*"
-        />
-      </div>
-      <button
-        className="bg-black w-32 mt-5 rounded-md text-white"
-        type="submit"
+const Contact = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = data => {
+    // TODO change the console log for modals or something to alert the user
+    axios({ method: 'POST', url: 'https://formspree.io/mwkrdvon', data })
+      .then(response => console.log(response?.data?.ok))
+      .catch(error => console.log(error));
+  };
+
+  // TODO implement whatever notification error you want
+  const errorStyle = name =>
+    errors[name]?.message ? 'border border-red-900' : '';
+
+  return (
+    <SectionLayout id="contact" className="items-center bg-blue-600 text-black">
+      <h2>Do you have a great idea you want to turn into a business?</h2>
+      <span>Let&apos;s talk, together we can achieve great things.</span>
+      <form
+        className="m-10 max-w-2xl flex flex-col w-full items-center"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        Submit
-      </button>
-    </form>
-  </SectionLayout>
-);
+        <div className="w-full grid gap-3 text-black">
+          <input
+            className={`placeholder-black p-2 ${errorStyle('name')}`}
+            placeholder="Name*"
+            name="name"
+            ref={register({ required: 'This field is required' })}
+          />
+          <input
+            className={`placeholder-black p-2 ${errorStyle('phone')}`}
+            placeholder="Phone number"
+            name="phone"
+            ref={register}
+          />
+          <input
+            className={`placeholder-black p-2 ${errorStyle('email')}`}
+            placeholder="Email*"
+            name="email"
+            ref={register({
+              required: 'This field is required',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: 'Entered value does not match email format'
+              }
+            })}
+          />
+          <textarea
+            className={`placeholder-black p-2 ${errorStyle('comments')}`}
+            placeholder="Tell us briefly about your project*"
+            name="comments"
+            ref={register({ required: 'This field is required' })}
+          />
+        </div>
+        <button
+          className="bg-black w-32 mt-5 rounded-md text-white"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </SectionLayout>
+  );
+};
 
 export default Contact;
