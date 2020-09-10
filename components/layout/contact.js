c48ac331b70f22df76d482d7c6d417b979b3fec1
dastasoft <45243062+dastasoft@users.noreply.github.com>
2020-09-10 12:32:17 +0200

    toast instead of modal, error messages in contact

--- components/layout/contact.js
+++ components/layout/contact.js
@@ -1,39 +1,43 @@
-import { useContext, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useContext } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import axios from 'axios';
-import Modal from 'react-modal';
-import { Button, Input, Textarea } from '@chakra-ui/core';
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  Textarea,
+  useToast
+} from '@chakra-ui/core';
 
 import { LanguageContext } from '../../context/language';
 import SectionLayout from '../sectionLayout';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
-};
-
 const Contact = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [modalContent, setModalContext] = useState(null);
-  const { register, handleSubmit, reset } = useForm();
+  const toast = useToast();
+  const { control, errors, register, handleSubmit, reset } = useForm();
   const { getTranslation } = useContext(LanguageContext);
 
   const onSubmit = data => {
     axios({ method: 'POST', url: 'https://formspree.io/mwkrdvon', data })
       .then(() => {
-        setModalContext(<div>{getTranslation('[ContactOK]')}</div>);
-        setIsOpen(true);
+        toast({
+          title: 'Ok',
+          description: getTranslation('[ContactOK]'),
+          status: 'success',
+          duration: 2000,
+          isClosable: true
+        });
         reset();
       })
       .catch(() => {
-        setModalContext(<div>{getTranslation('[ContactKO]')}</div>);
-        setIsOpen(true);
+        toast({
+          title: 'Error',
+          description: getTranslation('[ContactKO]'),
+          status: 'error',
+          duration: 2000,
+          isClosable: true
+        });
       });
   };
 
@@ -45,36 +49,61 @@ const Contact = () => {
         className="m-10 max-w-2xl flex flex-col w-full items-center"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="w-full grid gap-3 text-black">
-          <Input
-            placeholder={`${getTranslation('[Name]')}*`}
-            name="name"
-            ref={register()}
-            maxLength={256}
-            required
-          />
+        <div className="w-full grid gap-4 text-black">
+          <FormControl isInvalid={errors.name}>
+            <Controller
+              as={Input}
+              placeholder={`${getTranslation('[Name]')}*`}
+              name="name"
+              rules={{
+                required: getTranslation('[Required Field]')
+              }}
+              maxLength={128}
+              control={control}
+              isInvalid={errors.name}
+              focusBorderColor="#000"
+              errorBorderColor="red.400"
+            />
+            <FormErrorMessage>{errors?.name?.message}</FormErrorMessage>
+          </FormControl>
           <Input
             placeholder={getTranslation('[Phone number]')}
             name="phone"
             ref={register}
-            maxLength={256}
+            focusBorderColor="#000"
+            maxLength={128}
           />
-          <Input
-            placeholder={`${getTranslation('[Email]')}*`}
-            name="email"
-            ref={register()}
-            type="email"
-            maxLength={256}
-            required
-          />
-          <Textarea
-            style={{ resize: 'none' }}
-            placeholder={`${getTranslation('[Comments]')}*`}
-            name="comments"
-            ref={register()}
-            maxLength={256}
-            required
-          />
+          <FormControl isInvalid={errors.email}>
+            <Input
+              placeholder={`${getTranslation('[Email]')}*`}
+              name="email"
+              ref={register({
+                required: getTranslation('[Required Field]'),
+                pattern: {
+                  value: /^\S+@\S+\.\S+$/,
+                  message: getTranslation('[Email validation]')
+                }
+              })}
+              type="email"
+              focusBorderColor="#000"
+              errorBorderColor="red.400"
+              maxLength={128}
+            />
+            <FormErrorMessage>{errors?.email?.message}</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.comments}>
+            <Textarea
+              style={{ resize: 'none' }}
+              placeholder={`${getTranslation('[Comments]')}*`}
+              name="comments"
+              ref={register({ required: getTranslation('[Required Field]') })}
+              maxLength={256}
+              focusBorderColor="#000"
+              errorBorderColor="red.400"
+              isInvalid={errors.comments}
+            />
+            <FormErrorMessage>{errors?.comments?.message}</FormErrorMessage>
+          </FormControl>
         </div>
         <Button
           className="mt-5"
@@ -86,13 +115,6 @@ const Contact = () => {
           {getTranslation('[Submit]')}
         </Button>
       </form>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setIsOpen(false)}
-        style={customStyles}
-      >
-        {modalContent}
-      </Modal>
     </SectionLayout>
   );
 };
