9a60806de1b72334096d52af15ee9d8fc4fc8538
rubenofen <rubenofen@gmail.com>
2020-09-11 11:59:33 +0200

    Favicon + banner cookies

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useToast, Button } from '@chakra-ui/core';
 
 import MainSlide from '../components/sections/mainSlide';
 import Expertise from '../components/sections/expertise';
@@ -7,6 +9,29 @@ import Layout from '../components/layout';
 import { getAllMarkDownData } from '../lib/markDowns';
 
 export default function Home({ allProjectsData }) {
+  const toast = useToast();
+
+  useEffect(() => {
+    toast({
+      title: 'Esta web utiliza cookies',
+      status: 'info',
+      description: (
+        <>
+          <span className="mr-10">Prueba descripción</span>
+          <Button
+            className="text-blue-600 border border-white mr-2"
+            variantColor="transparent"
+          >
+            Acceptar
+          </Button>
+          <Button className="border border-white" variantColor="transparent">
+            Rechazar
+          </Button>
+        </>
+      ),
+      duration: null
+    });
+  }, []);
   return (
     <Layout>
       <Head>
