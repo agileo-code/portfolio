e09b97d014c81d1fd8d5010e036f85ca00c73926
rubenofen <rubenofen@gmail.com>
2020-09-11 14:09:36 +0200

    Banner traducido...no va

--- hooks/useBannerCookies.js
+++ hooks/useBannerCookies.js
@@ -2,10 +2,33 @@ import { useEffect, useContext } from 'react';
 import { useToast, Button } from '@chakra-ui/core';
 import { LanguageContext } from '../context/language';
 
+const renderToast = (onClose, getTranslation) => {
+  const onBannerAccept = () => localStorage.setItem('cookiesAccepted', true);
+  localStorage.setItem('bannerCookieOpened', true);
+  return (
+    <div className="w-screen bg-blue-700 text-white p-3">
+      <div className="flex flex-col items-center justify-between lg:flex-row">
+        <span>{getTranslation('[Coockie banner text]')}</span>
+        <div>
+          <Button
+            className="text-blue-600 border border-white mr-2"
+            variantColor="transparent"
+            onClick={() => {
+              onBannerAccept();
+              onClose();
+            }}
+          >
+            {getTranslation('[Accept]')}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const useBannerCookies = () => {
   const toast = useToast();
   const { getTranslation } = useContext(LanguageContext);
-  const onBannerAccept = () => localStorage.setItem('cookiesAccepted', true);
 
   useEffect(() => {
     setTimeout(() => {
@@ -17,28 +40,7 @@ const useBannerCookies = () => {
       if (!cookiesAccepted && !bannerCookieOpened) {
         toast({
           duration: null,
-          render: ({ onClose }) => {
-            localStorage.setItem('bannerCookieOpened', true);
-            return (
-              <div className="w-screen bg-blue-700 text-white p-3">
-                <div className="flex flex-col items-center justify-between lg:flex-row">
-                  <span>{getTranslation('[Coockie banner text]')}</span>
-                  <div>
-                    <Button
-                      className="text-blue-600 border border-white mr-2"
-                      variantColor="transparent"
-                      onClick={() => {
-                        onBannerAccept();
-                        onClose();
-                      }}
-                    >
-                      Aceptar
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            );
-          }
+          render: ({ onClose }) => renderToast(onClose, getTranslation)
         });
       }
     }, 1000);
