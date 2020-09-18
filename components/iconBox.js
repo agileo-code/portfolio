51aa4cf2f2064c12a60605bd2eec65772bde62cc
dastasoft <dastasoft@gmail.com>
2020-09-18 11:36:32 +0200

    rework services

--- components/iconBox.js
+++ components/iconBox.js
@@ -1,35 +1,38 @@
 import { useContext } from 'react';
 import Link from 'next/link';
-import { Button } from '@chakra-ui/core';
 
 import { LanguageContext } from '../context/language';
 
-const IconBox = ({ icon, title, desc, className, link }) => {
+const IconBox = ({ icon, title, desc, link }) => {
   const { getTranslation } = useContext(LanguageContext);
 
   return (
-    <div
-      className={`flex flex-col justify-between text-center md:text-left ${className}`}
-    >
-      <div className="flex flex-col items-center md:flex-row">
-        <div className="w-20 h-20 md:w-10 md:h-10">{icon}</div>
-        <h3 className="m-2  text-gray-900">{title}</h3>
+    <div className="p-4 md:mb-0 mb-6 flex flex-col text-center items-center">
+      <div className="w-20 h-20 inline-flex items-center justify-center rounded-full border border-blue-500 text-blue-600 mb-5 flex-shrink-0">
+        <div className="w-10 h-10 text-black">{icon}</div>
       </div>
-      <span className="text-sm text-gray-700">{desc}</span>
-      <div className="flex justify-center">
-        {link ? (
+      <div className="flex-grow">
+        <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
+          {title}
+        </h2>
+        <p className="leading-relaxed text-base text-gray-700">{desc}</p>
+        {link && (
           <Link href={link}>
-            <Button
-              className=""
-              variant="outline"
-              borderColor="#00B4D8"
-              _hover={{ bg: 'black', color: 'white' }}
-            >
+            <a className="mt-3 text-blue-600 inline-flex items-center">
               {getTranslation('[More info]')}
-            </Button>
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </a>
           </Link>
-        ) : (
-          <div />
         )}
       </div>
     </div>
