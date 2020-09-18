58c50260d4cf21dbe608b68aa7f06a64a2b22071
dastasoft <dastasoft@gmail.com>
2020-09-18 15:19:22 +0200

    rework training prices

--- components/projectBox.js
+++ components/projectBox.js
@@ -1,37 +1,40 @@
 import { useContext } from 'react';
-import Link from 'next/link';
 
 import { LanguageContext } from '../context/language';
 
-const ProjectBox = ({ icon, title, desc, className, url, id }) => {
+const ProjectBox = ({ icon, title, desc, url }) => {
   const { getTranslation } = useContext(LanguageContext);
 
   return (
-    <div
-      className={`flex flex-col items-center justify-center text-center ${className}`}
-    >
-      <div className="w-full h-full">
+    <div className="flex flex-col items-center justify-center text-center border border-gray-300 p-6 rounded-lg bg-white shadow-xl">
+      <div className="rounded-lg overflow-hidden">
         <img src={icon} alt={`${title} Thumb`} />
       </div>
-      <h3 className="m-2">{title}</h3>
-      <span>{desc}</span>
-      <div className="flex justify-center py-3">
-        {url && (
-          <a
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-            className="inline-flex text-white bg-corporative-blue border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded text-lg"
+      <h2 className="text-xl font-medium title-font text-gray-900 mt-5">
+        {title}
+      </h2>
+      <p className="text-base mt-2">{desc}</p>
+      {url && (
+        <a
+          href={url}
+          rel="noreferrer"
+          target="_blank"
+          className="text-blue-600 inline-flex items-center mt-3 cursor-pointer"
+        >
+          {getTranslation('[View Demo]')}
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
           >
-            {getTranslation('[View Demo]')}
-          </a>
-        )}
-        <Link href="/projects/[id]" as={`/projects/${id}`}>
-          <a className="ml-4 inline-flex text-gray-800 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
-            {getTranslation('[More info]')}
-          </a>
-        </Link>
-      </div>
+            <path d="M5 12h14M12 5l7 7-7 7" />
+          </svg>
+        </a>
+      )}
     </div>
   );
 };
--- components/sectionLayout.js
+++ components/sectionLayout.js
@@ -1,7 +1,8 @@
-const sectionLayout = ({ id, children, className }) => (
+const sectionLayout = ({ id, children, className, style }) => (
   <div
     id={id}
     className={`${className} flex flex-col justify-around relative p-2 pt-20 pb-20`}
+    style={style}
   >
     {children}
   </div>
