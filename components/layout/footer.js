11ab875d107a020d21ff9f9cb2ceea8a3baed662
dastasoft <dastasoft@gmail.com>
2020-09-18 12:02:26 +0200

    footer reworked

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -4,19 +4,96 @@ import { LanguageContext } from '../../context/language';
 
 const Footer = () => {
   const { getTranslation } = useContext(LanguageContext);
+
   return (
-    <div className="bg-black text-corporative-color flex justify-between items-center p-6">
-      Nimbel© {new Date().getFullYear()} Copyright, Sant Cugat del Vallès,
-      Barcelona
-      <div className="flex flex-col text-gray-500 text-xs">
-        <Link href="privacy">
-          <a rel="nofollow">{getTranslation('[Privacy policy]')}</a>
-        </Link>
-        <Link href="termsAndConditions">
-          <a rel="nofollow">{getTranslation('[Terms of service]')}</a>
-        </Link>
+    <footer className="text-gray-700 body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <img
+            src="/images/nimbel.svg"
+            className="w-20 cursor-pointer"
+            alt="Nimbel"
+          />
+        </a>
+        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+          © {new Date().getFullYear()} Nimbel —
+          <a
+            href="https://twitter.com"
+            className="text-gray-600 ml-1"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            @nimbel
+          </a>
+        </p>
+
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center items-center sm:justify-start">
+          <a className="text-gray-500">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-500">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-500">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-500">
+            <svg
+              fill="currentColor"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="0"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="none"
+                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+              />
+              <circle cx="4" cy="4" r="2" stroke="none" />
+            </svg>
+          </a>
+          <div className="flex flex-col text-gray-500 text-xs ml-8">
+            <Link href="privacy">
+              <a rel="nofollow">{getTranslation('[Privacy policy]')}</a>
+            </Link>
+            <Link href="termsAndConditions">
+              <a rel="nofollow">{getTranslation('[Terms of service]')}</a>
+            </Link>
+          </div>
+        </span>
       </div>
-    </div>
+    </footer>
   );
 };
 
