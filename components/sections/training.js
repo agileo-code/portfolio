14eb0d86fd681e8271ca37a7d962ba785a158598
dastasoft <dastasoft@gmail.com>
2020-09-23 15:12:56 +0200

    fixed accesibility problems

--- components/sections/training.js
+++ components/sections/training.js
@@ -47,7 +47,7 @@ const Expertise = () => {
             <button
               type="button"
               onClick={() => onClick(router, 'contact')}
-              className="inline-flex text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded text-lg"
+              className="inline-flex text-white bg-corporative-blue border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded text-lg"
             >
               {getTranslation('[Contact]')}
             </button>
