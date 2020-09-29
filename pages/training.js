0bbe3f2247dd03b9ea552c9c68b13fc15484cfff
rubenofen <rubenofen@gmail.com>
2020-09-29 13:16:44 +0200

    Prueba programas de formación

--- pages/training.js
+++ pages/training.js
@@ -26,12 +26,12 @@ export default function Training() {
         backgroundSize: 'contain'
       }}
     >
-      <div className="grid md:grid-cols-2 mt-10">
-        <div className="md:grid md:grid-rows-2 flex justify-center">
+      <div className="grid md:grid-cols-2 md:m-10">
+        <div className="md:grid md:grid-rows-2 flex justify-center md:justify-start">
           <h2 className="self-center mb-5 md:mb-0">
             {getTranslation('[Training Programs]')}
           </h2>
-          <IdeaPerson className="w-1/4 justify-self-center self-start hidden md:block" />
+          <IdeaPerson className="w-2/4 justify-self-end self-start hidden md:block" />
         </div>
         <WhiteBoard />
       </div>
