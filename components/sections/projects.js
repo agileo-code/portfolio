de0644e135e4d61e911dd2e5f89fd3c349b6f161
dastasoft <dastasoft@gmail.com>
2020-09-30 17:49:25 +0200

    fixed header for proyectos

--- components/sections/projects.js
+++ components/sections/projects.js
@@ -10,16 +10,16 @@ export default function Projects({ allProjectsData }) {
     <section className="text-gray-700 bg-gray-100 body-font" id="projects">
       <div className="container px-5 py-12 mx-auto">
         <div className="flex flex-col">
-          <div className="h-1 bg-gray-200 rounded overflow-hidden">
-            <div className="w-24 h-full bg-corporative-blue" />
-          </div>
-          <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-            <h1 className="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">
+          <div className="text-center mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
               {getTranslation('[Projects]')}
             </h1>
-            <p className="sm:w-3/5 text-base sm:pl-10 pl-0">
+            <p className="text-base leading-relaxed w-5/6 lg:w-2/3 mx-auto">
               {getTranslation('[Projects desc]')}
             </p>
+            <div className="flex mt-6 justify-center">
+              <div className="w-16 h-1 rounded-full bg-blue-500 inline-flex" />
+            </div>
           </div>
         </div>
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
