bcf76c14f8fb3629e8076d582a41b90af6aaad68
rubenofen <rubenofen@gmail.com>
2020-10-08 10:51:49 +0200

    Link no crawleable

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -10,9 +10,11 @@ const Footer = () => {
   return (
     <footer className="text-white body-font bg-black">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center">
-          <Logo className="w-20 cursor-pointer" />
-        </a>
+        <Link href="/">
+          <a className="flex title-font font-medium items-center md:justify-start justify-center">
+            <Logo className="w-20 cursor-pointer" />
+          </a>
+        </Link>
         <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © {new Date().getFullYear()} Nimbel, Sant Cugat del Vallès, Barcelona,
           Spain
