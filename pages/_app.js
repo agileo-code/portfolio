e54c5de2e6947b34b2cdead88245cae2fecfafee
rubenofen <rubenofen@gmail.com>
2020-11-13 13:44:34 +0100

    NO local storage too

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
-import { useEffect } from 'react';
 import GlobalProviders from '../components/globalProviders';
 
 import '../styles/index.css';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    localStorage.setItem('bannerCookieOpened', false);
-  }, []);
-
   return (
     <GlobalProviders>
       <Component {...pageProps} />
